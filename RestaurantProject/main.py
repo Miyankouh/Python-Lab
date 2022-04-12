9a201b93d9467d5cde6d248c960e7c90e129f0b9
Miyankouh <majid.miyankouh@gmail.com>
2022-04-12 10:40:57 +0430

    add DataBase in Resturant project

--- RestaurantProject/main.py
+++ RestaurantProject/main.py
@@ -1,10 +1,61 @@
 from tkinter import *
-
 from tkinter.font import *
+import os
+
+import sqlite3
+
+#region data base class
+############################################################################# DB
+import sqlite3
+
+class Database:
+    def __init__(self, db):
+        self.__db_name = db
+        self.connection = sqlite3.connect(db)
+        self.cursor = self.connection.cursor()
+        
+        self.cursor.execute("""
+                            CREATE TABLE IF NOT EXISTS [table_menu](
+                            [ID] INT PRIMARY KEY NOT NULL UNIQUE, 
+                            [name] NVARCHAR(50) NOT NULL UNIQUE, 
+                            [price] INT NOT NULL,
+                            [is_food] BOOL NOT NULL) WITHOUT ROWID;
+                            """)
+        
+        self.connection.commit()
+        self.connection.close()
+        
+        
+    
+    def insert(self, id, name, price,is_food):
+        self.connection = sqlite3.connect(self.__db_name)
+        self.cursor = self.connection.cursor()
+        self.cursor.execute("INSERT INTO table_menu VALUES (? , ? , ? , ?)" , (id, name, price, is_food))       
+        self.connection.commit()
+        self.connection.close()
+        
+    
+    def get_menu_items(self, is_food):
+        self.connection = sqlite3.connect(self.__db_name)
+        self.cursor = self.connection.cursor()
+        self.cursor.execute("SELECT * FROM table_menu WHERE is_food = ?" , is_food)
+        result = self.cursor.fetchall()
+        return result
+    
+############################################################################# End of DB
+#endregion
+
+if os.path.isfile('restaurant.db') == False:
+    db = Database('restaurant.db')
+    db.insert(1, 'چلومرغ', 22000, True)
+    db.insert(2, 'زرشک پلو ساده', 17000, True)
+    db.insert(3, 'باقالی پلو با گوشت', 67000, True)
+    db.insert(4, 'نوشابه قوطی', 3000, False)
+    db.insert(5, 'نوشابه خانواده', 5000, False)
+    db.insert(6, 'دوغ قوطی', 4000, False)
 
 
 # start
-
 window = Tk()
 
 
@@ -15,17 +66,13 @@ height = window.winfo_screenheight()
 
 
 # Display the project on the page
-
 window.geometry('%dx%d' % (width, height))
-
 
 window.grid_columnconfigure(0, weight=2)
 
 window.grid_columnconfigure(1, weight=3)
 
-
 window.grid_rowconfigure(0, weight=1)
-
 
 window.state('zoomed') # maximize
 
@@ -38,13 +85,14 @@ window.title('نرم افزار مدیریت رستوران')
 
 
 # font size
-
 vazir_font = Font(family='Arial', size=16)
 
 
 
 #----------Invoice frames------------------------------------------------------
 
+#region receipt
+###################################################### 
 
 # receipt_frame
 receipt_frame = LabelFrame(window, text='صورت حساب', font=vazir_font, padx=pad_x, pady=pad_y)
@@ -69,21 +117,26 @@ list_box_frame.grid_columnconfigure(1, weight=1)
 list_box_frame.grid_columnconfigure(2, weight=1)
 list_box_frame.grid_columnconfigure(3, weight=1)
 
+# Delete button in list_box
 button_delete = Button(list_box_frame, text='حذف سطر', font=vazir_font)
 button_delete.grid(row=0, column=0, sticky='nsew')
 
+# new button in list_box
 button_new = Button(list_box_frame, text=' فاکتور جدید', font=vazir_font)
 button_new.grid(row=0, column=1, sticky='nsew')
 
+# Add button in list_box
 button_add = Button(list_box_frame, text='+', font=vazir_font)
 button_add.grid(row=0, column=2, sticky='nsew')
 
+# Minus button in list_box
 button_minus = Button(list_box_frame, text=' -', font=vazir_font)
 button_minus.grid(row=0, column=3, sticky='nsew')
+# ###################################################### 
+#endregion
 
 
-
-# menu frame
+# Menu frame
 menu_frame = LabelFrame(window, text='منو نوشیدنی و غذاها', font=vazir_font, padx=pad_x, pady=pad_y)
 menu_frame.grid(row=0, column=1, sticky='nsew', padx=pad_x, pady=pad_y)
 
@@ -92,23 +145,24 @@ menu_frame.grid_columnconfigure(1, weight=2)
 
 menu_frame.grid_rowconfigure(0, weight=1)
 
-# Drink frame
+# Drink frame in Menu frame
 drink_frame = LabelFrame(menu_frame, text="نوشیدنی ها", padx=pad_x, pady=pad_y)
 drink_frame.grid(row=0, column=0, sticky='nsew', padx=pad_x, pady=pad_y)
 
-# Food frame
+# Food frame in Menu frame
 food_frame = LabelFrame(menu_frame, text=" غذا ها", padx=pad_x, pady=pad_y)
 food_frame.grid(row=0, column=1, sticky='nsew', padx=pad_x, pady=pad_y)
 
 
-# button frame
+# Button frame
 buttons_frame = LabelFrame(window, font=vazir_font, padx=pad_x, pady=pad_y)
 buttons_frame.grid(row=1, column=1, padx=pad_x, pady=pad_y)
 
-
+# Exit in Button frame
 button_exit = Button(buttons_frame, text='خروج')
 button_exit.grid(row=0, column=0)
 
+# Calculate in Button frame
 button_calculate = Button(buttons_frame, text='ماشین حساب')
 button_calculate.grid(row=0, column=1)
 
@@ -117,5 +171,4 @@ button_calculate.grid(row=0, column=1)
 
 
 # end
-
 window.mainloop()
