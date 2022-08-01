6bba2e98ffee07d033084b39754da016c89464b0
Miyankouh <majid.miyankouh@gmail.com>
2022-08-01 15:02:33 +0430

    add: Linear search

---
+++ Algorithm/linear_search.py
@@ -0,0 +1,15 @@
+"""
+    Linear(Sequential) search
+        [1, 2, 5, 8, 11, 14, 18, 23, 32, 48], 11 => 4
+"""
+
+def linear_search(array, element):
+    for i in range(len(array)):
+        if array[i] == element:
+            return i
+    
+    return -1
+
+
+if __name__ == '__main__':
+    print(linear_search([1, 2, 5, 8, 11, 14, 18, 23, 32, 48], 11))
