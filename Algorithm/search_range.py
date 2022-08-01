c44c819a9991a059584be5787041f35c4a8dfc6c
Miyankouh <majid.miyankouh@gmail.com>
2022-08-01 14:37:30 +0430

    add: search range

---
+++ Algorithm/search_range.py
@@ -0,0 +1,28 @@
+"""  
+    search range
+        [5, 7, 7, 8, 8, 8, 10], 8 => [3, 5]
+        [5, 7, 7, 8, 8, 8, 10], 11  => [None, None]
+"""
+
+def search_range(nums, target):
+    low = 0
+    high = len(nums) - 1
+
+    while low <= high:
+        mid = low + (high - low) // 2
+        if target < nums[mid]:
+            high = mid - 1
+        elif target > nums[mid]:
+            low = mid + 1
+        else:
+            break
+
+    for j in range(len(nums) - 1, -1, -1):
+        if nums[j] == target:
+            return [mid, j]
+        
+    return [None, None]
+
+
+if __name__ == '__main__':
+    print(search_range([5, 7, 7, 8, 8, 8, 10], 7))
