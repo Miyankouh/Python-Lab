b43cc4159ad6206007cb562dea2064b2ec185939
Miyankouh <majid.miyankouh@gmail.com>
2022-08-14 13:23:46 +0430

    test: add test {split_into function}

---
+++ Python-test/split_into/main.py
@@ -0,0 +1,11 @@
+from itertools import islice
+
+
+def split_into(iterable, sizes):
+    it = iter(iterable)
+    for size in sizes:
+        if size is None:
+            yield list(it)
+            return
+        else:
+            yield list(islice(it, size))
