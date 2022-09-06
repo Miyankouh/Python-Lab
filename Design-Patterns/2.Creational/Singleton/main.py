d54ddfad0f1ba7e433d02d719b09a98fa0015fed
miyankouh <majid.miyankouh@gmail.com>
2022-09-06 15:56:49 +0430

    refactor: __SingletonObject to __SingletonObject()

--- Design-Patterns/2.Creational/Singleton/main.py
+++ Design-Patterns/2.Creational/Singleton/main.py
@@ -1,16 +1,15 @@
 class SingletonObject(object):
-    
     class __SingletonObject():
         def __init__(self):
             self.file_name = None
-        
-        def __str__(self) -> str:
+
+        def __str__(self):
             return "{0!r} {1}".format(self, self.file_name)
 
         def _write_log(self, level, msg):
             with open(self.file_name, "a") as log_file:
                 log_file.write("[{0}] {1}\n".format(level, msg))
-        
+
         def critical(self, msg):
             self._write_log("CRITICAL", msg)
 
@@ -30,12 +29,12 @@ class SingletonObject(object):
 
     def __new__(cls):
         if not SingletonObject.instance:
-            SingletonObject.instance = SingletonObject.__SingletonObject
-        
+            SingletonObject.instance = SingletonObject.__SingletonObject()
+
         return SingletonObject.instance
-    
+
     def __getattr__(self, name):
         return getattr(self.instance, name)
-    
+
     def __setattr__(self, name):
         return setattr(self.instance, name)
